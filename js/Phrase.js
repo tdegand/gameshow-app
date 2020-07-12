528bbce351c1d026a77806a46d56b25fdfa653dc
Tyler Degand <tylerdegand@gmail.com>
2020-07-12 12:31:28 -0500

    putting all folders in correct one

---
+++ js/Phrase.js
@@ -0,0 +1,25 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+/* Create the "phrase' class
+*  @phrase is a class that uses a constructor to create a class with properties
+* */
+
+class phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+
+    }
+
+    checkLetter() {
+
+    }
+
+    chowMatchedLetter() {
+
+    }
+}

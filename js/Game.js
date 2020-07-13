396e4add834764d4897869179d584d99f04c6d01
Tyler Degand <tylerdegand@gmail.com>
2020-07-13 10:53:42 -0500

    not sure I'm doing this right. Will review and test my code

--- js/Game.js
+++ js/Game.js
@@ -35,18 +35,75 @@ class game {
 
     }
 
-    handleInteraction() {
+    /**
+     * 
+     * @param {string} letter represents the letters of phrase
+     */
+    handleInteraction(letter) {
+        const key = document.getElementsByClassName('key');
+        //loop over the keys and check each one
+        key.forEach(key => {
+            if (this.key) {
+                this.key.disabled;
+            }
+            //if selected key does not match remove a life adn add the class "wrong" 
+            if (this.key.innerHTML !== letter) {
+                this.key.classname = 'wrong';
+                this.removeLife(miss);
+            } else {
+                //if key does match add class of "chosen" show the matched letter and check for a win
+                this.key.classname = 'chosen';
+                this.showMatchedLetter();
+                return this.checkForWin();
+            }
+            //continue to check if the player has won or not
+            if (this.checkForWin() === true) {
+                return this.gameOver();
+            }
+        })
+    }
+    /**
+     * Remove a heart if user is incorrect on guessing
+     * @param {boolean} miss return true if user has missed, false if user is correct
+     */
+    removeLife(miss) {
+        const hearts = document.querySelectorAll('.tries');
+        hearts.forEach(heart => {
+            if (miss) {
+                heart.setAttribute('src', 'images/lostHeart.png');
+            }
+            this.missed++
+        })
 
     }
 
-    removeLife() {
-
-    }
+    //checks to see if the user has won or not by revealing all letters or has any remaining hearts
     checkForWin() {
-
+        const phraseContainer = document.getElementById('phrase ul');
+        phraseContainer.children.forEach(letter => {
+            if (letter.classname === 'show') {
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
-
-    gameOver() {
-
+    /**
+     * win the game is over it displays the overlay again
+     * if the player wins it displays a message and updates the overlay
+     * if player loses it updates the message to a new message and the overlay class
+     * @param {boolean} win  returns true if user wins, false is user loses
+     */
+    gameOver(win) {
+        const overlay = document.getElementById('overlay');
+        const gameMessage = document.getElementById('game-over-message');
+        overlay.style.display = 'block';
+        if (win) {
+            gameMessage.innerHTML = `You win!`;
+            overlay.classname = 'win';
+        } else {
+            gameMessage.innerHTML = `You lost...`;
+            overlay.classname = 'lose';
+        }
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
